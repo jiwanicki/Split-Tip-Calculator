74d516b0cc036c0821d604462deac06ba8d8065f
sfhome1 <sfhome1@icloud.com>
2014-12-08 18:57:47 -0800

    second  rev project

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var checkAmountText: UITextField!
+    @IBOutlet weak var taxAmountText: UITextField!
+    
+    @IBOutlet weak var mySlider: UISlider!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func sliderValueChanged(sender: UISlider) {
+        if (sender == _slider) {
+            _myLabel.text = [NSString stringwithFormat:@"%0.3f", _slider.value];
+        }
+    }
+    @IBAction func pressButtonSend(sender: UIButton) {
+   
+    
+    
+    }
 }
 
